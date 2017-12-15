f5a487f353284f66b5f108e79fea849149d3b844
KozhevnikovIvan <32384646+KozhevnikovIvan@users.noreply.github.com>
2017-12-15 10:59:29 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -139,7 +139,7 @@ void multiplication_matrix(int **&Matrix, int x, int y) {
 
 void transpose_matrix(int **&Matrix, int &x, int &y) {
   if (Matrix == nullptr)
-    cout << "Matrix is empty" << endl;
+    cout << "Матрица пуста" << endl;
   else {
     int **Matrix_tr;
     Matrix_tr = new int *[x];
